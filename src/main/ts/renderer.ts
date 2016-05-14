787fa4672361f026273c7938e69e25aa996b7852
Thomas Krämer <thomas.kraemer@posteo.de>
2016-05-14 15:42:54 +0200

    parse single version 1 record

--- src/main/ts/renderer.ts
+++ src/main/ts/renderer.ts
@@ -3,23 +3,23 @@
 // All of the Node.js APIs are available in this process.
 
 let holder = document.getElementById('holder');
-holder.ondragover = function () {
+holder.ondragover = () => {
     return false;
 };
 
-holder.ondragleave = holder.ondragend = function () {
+holder.ondragleave = holder.ondragend = () => {
     return false;
 };
 
-holder.ondrop = function (e) {
+holder.ondrop = (e) => {
     e.preventDefault();
-    var file = e.dataTransfer.files[0];
-    console.log('File you dragged here is', file.path);
+    let file = e.dataTransfer.files[0];
+    console.log('File you dragged here is', file['path']);
 
-    var fs = require('fs');
-    fs.readFile(file.path, 'utf8', function (err, data) {
-        if (err) return console.log(err);
-
+    let fs = require('fs');
+    fs.readFile(file['path'], 'utf8', (err, data) => {
+        if (err)
+            return console.log(err);
         console.log(data);
     });
 
@@ -27,9 +27,41 @@ holder.ondrop = function (e) {
 };
 
 interface TroopData {
-
+    name: String;
+    displayName: String;
+    displayNamePlural: String;
+    factionId: String;
+    flags: Number;
+    upgrade1: Number;
+    upgrade2: Number;
+    // strength: Number;
+    // agility: Number;
+    // intelligence: Number;
+    // charisma: Number;
+    // level: Number;
+    // prof1h: Number;
+    // prof2h: Number;
+    // profPolearms: Number;
+    // profArcher: Number;
+    // profCrossbow: Number;
+    // profThrowing: Number;
+    // profFire: Number;
 }
 
-function loadTroopData(input: String): TroopData {
-    return null;
+function getTroopData(input: String): TroopData {
+    let lines: string[][] = [];
+    input.split('\n').forEach((line, i) => lines.push(line.split(' ')));
+
+    if (lines.length != 6)
+        throw 'incomplete troop data: unexpected line count';
+
+    return {
+        name: lines[0][0],
+        displayName: lines[0][1],
+        displayNamePlural: lines[0][2],
+        flags: parseInt(lines[0][3], 10),
+        factionId: lines[0][6],
+        upgrade1: parseInt(lines[0][7], 10),
+        upgrade2: parseInt(lines[0][8], 10)
+    };
 }
--- src/test/js/rendererTest.js
+++ src/test/js/rendererTest.js
@@ -1,6 +1,23 @@
 it('jasmine is working', function(){ expect(true).toEqual(true); });
 
-it('should return null', function(){
-    var result = loadTroopData('');
-    expect(result).toEqual(null);
+it('should load version 1 troop entry', function(){
+    var entry = "trp_swadian_militia Swadian_Militia Swadian_Militia 67108864 0 0 6 618 618\n" //
+    + "124 0 269 0 270 0 328 0 400 0 374 0 194 0 156 0 235 0 236 0 232 0 138 0 133 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 -1 0 \n" //
+    + "1 2 3 4 5\n" //
+    + "7 11 7 12 8 13 10\n" //
+    + "274 131072 0 1 0 0\n" //
+    + "17179869185 1315051091194281984 1835136 0 50465891015 7916447985573822463 2031036 0 ";  
+
+    var result = getTroopData(entry);
+    
+    var expected = {
+         name: 'trp_swadian_militia',
+         displayName: 'Swadian_Militia',
+         displayNamePlural: 'Swadian_Militia',
+         flags: 67108864,
+         factionId: '6',
+         upgrade1: 618,
+         upgrade2: 618
+        };
+    expect(result).toEqual(expected);
 });
